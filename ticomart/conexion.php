da26319c0691c17033c7b93c8c17c593176afb48
crandrescr <sprogramador.web@gmail.com>
2025-03-25 11:45:54 -0600

    Update conexion.php

--- ticomart/conexion.php
+++ ticomart/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = new mysqli("mysql://root:lZQUVHhUatZhUJDiPMqIQcdFTeyMriKB@mysql.railway.internal:3306/railway","root","lZQUVHhUatZhUJDiPMqIQcdFTeyMriKB","railway");
+$connect = new mysqli("mysql://root:lZQUVHhUatZhUJDiPMqIQcdFTeyMriKB@yamabiko.proxy.rlwy.net:26477/railway","mysql.railway.internal","lZQUVHhUatZhUJDiPMqIQcdFTeyMriKB","railway");
 
 if($connect){
 	 
